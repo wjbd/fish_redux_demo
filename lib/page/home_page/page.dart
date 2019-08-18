8bbddd4948204528372f552d629eda22ea00d587
lixingqu <wjbd66@aliyun.com>
2019-08-19 00:33:14 +0800

    删除多余代码

--- lib/page/home_page/page.dart
+++ lib/page/home_page/page.dart
@@ -1,7 +1,6 @@
 import 'package:fish_redux/fish_redux.dart';
 
 import 'effect.dart';
-import 'reducer.dart';
 import 'state.dart';
 import 'view.dart';
 import 'weather_adapter/adapter.dart';
@@ -11,7 +10,6 @@ class HomePage extends Page<HomeState, Map<String, dynamic>> {
       : super(
           initState: initState,
           effect: buildEffect(),
-          reducer: buildReducer(),
           view: buildView,
           dependencies: Dependencies<HomeState>(
             adapter: NoneConn<HomeState>() + WeatherAdapter(),
